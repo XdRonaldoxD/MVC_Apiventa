41104fe063d7b15a4a9d3073cc9c432dadc9679f
Ronaldo <smithxd118@gmail.com>
2022-12-22 14:21:57 -0500

    ok

--- Controllers/Api/NotificatationController.php
+++ Controllers/Api/NotificatationController.php
@@ -5,13 +5,13 @@ require_once "EloquentsModel/Notificacion.php";
 class NotificatationController{
 
     public function NotificarMercadoPago(){
-        $data = json_decode(file_get_contents('php://input'), true);
+       
         $datos=[
-            'json_notificacion'=>$data,
+            'json_notificacion'=>isset($_POST["type"])?$_POST["type"] : '',
             'fecha_creacion_notificacion'=>date('Y-m-d H:i:s')
         ];
         Notificacion::create($datos);
-        echo json_encode($data);
+        echo json_encode('ok');
     }
 
 }
